3356a170463d7fa2fd087dfb4772b1355876b629
Stacey King <stacey@netengine.com.au>
2012-08-31 14:36:53 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c30cd059e7a958708fca8d258ae3b49611aa429a7bd8c0f9cce0d6e8b0b429ae04e93a3658c0c498c874f160874660cdf755d3c55f4bb14221662b792ab239d1'
